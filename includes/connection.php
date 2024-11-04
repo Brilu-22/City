d0e560b951af4b9e154f601a5bdc00ee85908153
brilu-22 <231214@virtualwindow.co.za>
2024-11-04 17:40:25 +0200

    commit

--- includes/connection.php
+++ includes/connection.php
@@ -1,14 +1,23 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = " "; 
-$dbname = "meter_box_app"; 
+// Fetch environment variables
+$cloudSqlUser = getenv('CLOUDSQL_USER');
+$cloudSqlPassword = getenv('CLOUDSQL_PASSWORD');
+$cloudSqlDatabase = getenv('CLOUDSQL_DB');
+$cloudSqlDsn = getenv('CLOUDSQL_DSN');
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+// Create a new PDO instance
+try {
+    $pdo = new PDO("mysql:unix_socket={$cloudSqlDsn};dbname={$cloudSqlDatabase}", $cloudSqlUser, $cloudSqlPassword);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Handle connection error
+    echo "Connection failed: " . $e->getMessage();
+    exit();
 }
+
+// Optional: Set character set to UTF-8
+$pdo->exec("set names utf8");
+
+// Now you can use $pdo for your database operations
 ?>
